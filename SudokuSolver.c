7ca87ba8051bf0e29186fc7615f38c7f40e2187f
Kevin Focke <19843342+KevinFocke@users.noreply.github.com>
2022-02-10 13:48:56 +0100

    Refactored globals, created struct sudoku

--- SudokuSolver.c
+++ SudokuSolver.c
@@ -8,34 +8,21 @@
 #include <math.h>
 #include <regex.h>
 #include <stdlib.h>
-// Supported sudoku is 9 x 9
-int MAXWIDTH = 9; // Max width of sudoku;
-int MAXLENGTH = 9; // Max length of sudoku
-#define MAX(A,B) ((A)>(B))?(A):(B);
 
+// Supported sudoku is 9 x 9
+const int MAXDIMENSION = 9; // Max dimension of sudoku
+const int MAXARRAY = MAXDIMENSION * MAXDIMENSION; // MAXARRAY is the square of maxdimension.
+
+#define MAX(A,B) ((A)>(B))?(A):(B);
+// Sudoku is represented as a matrix
 struct sudoku{
-    int* dataMatrix;
-     // a 9 x 9 matrix contains 3 boxes on top, 3 boxes in middle, 3 in bottom
-    int dimension;
-    int numberCount;
+    int size;
+    int rowlength;
+    int collength;
+    int *matrix2D[MAXARRAY];
     };
 
-int checkDimensions(int *dimension1, int *dimension2)
-{
-    // Are the dimensions square?
-    if (*dimension1 == *dimension2)
-    {
-        return 0;
-    }
-    else
-    {
-        printf("Dimension weirdness. Sudoku's should have square dimensions \n Dim 1: %d\n Dim 2: %d \n", *dimension1, *dimension2);
-        return 1;
-        //     char filename[] = "sudoku_input.txt";return 1;
-    }
-}
-
-int readFile(char filename[], int *maxdimension, int *numbercount, struct sudoku *sud)
+int readFile(char filename[], int *numbercount, int sudoku_array[])
 {
     // TODO: Regex to set custom filename
     // readGit source control manager in the file
@@ -43,20 +30,10 @@ int readFile(char filename[], int *maxdimension, int *numbercount, struct sudoku
     printf("%s", filename);
     fp = fopen(filename, "r");
 
-    // Calc max possible array
-    int MAXARRAY = MAXWIDTH * MAXLENGTH;
-    *maxdimension = 0;
-    if (MAXWIDTH == MAXLENGTH){
-        *maxdimension = MAXWIDTH;
-    }
+
 
     *numbercount = 0;
     printf("Initializing Max Sudoku array.");
-    int sudoku_array[MAXARRAY]; // unsolved sudokus are zero. Unfilled sudoku elements are null.
-    for (int i = 0; i < MAXARRAY; i++)
-    {
-        sudoku_array[i] = -1; 
-    }
     printf("scanning %s\n", filename);
     // TODO : Detect non-numeric characters
     int buffer = 0;
@@ -75,45 +52,51 @@ int readFile(char filename[], int *maxdimension, int *numbercount, struct sudoku
     printf("%d", sudoku_array[i]);    
     }
     printf("\nCounted %d numbers \n", *numbercount);
-    
+
     return 0;
 }
 
 // TODO: Create a seperate function for reading in file
 int main(void){
-    // Check globals
-    if (checkDimensions(&MAXLENGTH,&MAXWIDTH))
-    {
-        return 1;
-    };
-
+    //TODO: Enabling queuing sudokus
     // Init vars
     char filename[] = "sudoku_input.txt";
-    int maxdimension, numbercount = {0};
-    //TODO: Enabling queuing sudokus
-    struct sudoku sud; 
-    // *psud = &sud; // create a sud, and a pointer to it
+    int numbercount = 0;
+    struct sudoku sud;
+    // Initialize the maximum possible sudoku array
+    int sudoku_array[MAXARRAY]; // unsolved sudokus are zero. Unfilled sudoku elements are null.
+    for (int i = 0; i < MAXARRAY; i++)
+    {
+        sudoku_array[i] = -1; 
+    }
     //Function calls
-    if (readFile(filename, &maxdimension, &numbercount, &sud))
+    if (readFile(filename, &numbercount, sudoku_array))
     {
         return 1;
     }
+    // Set sudoku
+    sud.size = numbercount;
+    sud.collength = sud.rowlength = MAXDIMENSION; //square dimensions
+
+
+
+    // Argument: struct sudoku *sud
     // Count the size of the input. Does it match known dimensions?
 
-    int DataMatrixDimension = 0; // Figure out the square dimension of the data
-    for (int i = 1; i <= maxdimension; i++)
+    int dataMatrixDimension = 0; // Figure out the square dimension of the data
+    for (int i = 1; i <= MAXDIMENSION; i++)
     {
         if (numbercount == i * i)
         {
-            DataMatrixDimension = i;
-            printf("The data dimension is %d x %d \n", DataMatrixDimension, DataMatrixDimension);
+            dataMatrixDimension = i;
+            printf("The data dimension is %d x %d \n", dataMatrixDimension, dataMatrixDimension);
             break;
         }
     }
 
-    if (DataMatrixDimension == 0)
+    if (dataMatrixDimension == 0)
     {
-        printf("Invalid numbercount. Numbers received: %d.", numbercount);
+        printf("Invalid numbercount; does not match square dimensions. Numbers received: %d.", numbercount);
         return 1;
     }
     // TODO: Free Malloc
