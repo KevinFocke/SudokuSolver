910bc3a658a878e91161b38e67308e53c9c600d7
Kevin Focke <19843342+KevinFocke@users.noreply.github.com>
2022-02-14 16:40:41 +0100

    feat solveSudoku dummy

--- SudokuSolver.c
+++ SudokuSolver.c
@@ -15,9 +15,13 @@ struct sudoku
     int size;
     int rowLength;
     int colLength;
+    int boxWidth; // Not compatible with snake matrix
+    int totalUnsolved;
     int **matrix; // 2D matrix, list of pointers to arrays containing digits
-    int **boxList; // list of pointers to Boxes, filling from top-left to right;
+    struct box **boxList; // list of pointers to Boxes, filling from top-left to right;
 };
+
+//BUG: Potential bug in **boxlist pointer logic
 
 
 struct box
@@ -87,22 +91,31 @@ int initBoxMatrix (struct box *box, struct sudoku *sud, int boxPosVertical, int 
     P P P
     */
 
+   // Memory allocation
    int ***tempBoxMatrix = (int ***)saferCalloc(boxWidth, sizeof(int**));
    for (int i = 0; i < (boxWidth); i++)
    {
        tempBoxMatrix[i] = (int**)saferCalloc(boxWidth, sizeof(int*));
    }
+
+    // Assign pointers + calculate unsolvedcount
+    int tempUnsolved = sud->colLength;
+
    for (int row = 0; row < boxWidth; row++)
    {
        for (int col = 0; col < boxWidth; col++)
        {
            tempBoxMatrix[row][col] = &(sud->matrix[row+(boxPosVertical*3)][col+(boxPosHorizontal*3)]);
-           printf("%i", *(tempBoxMatrix[row][col]));
+            int curval = *(tempBoxMatrix[row][col]);
+            if (curval != 0)
+            {
+                tempUnsolved -= 1;
+            }
+
        }
-       printf("\n");
    }
-    printf("-\n");
     box->pointerMatrix = tempBoxMatrix;
+    box->unsolvedCount = tempUnsolved;
 
     return 0;
 }
@@ -110,6 +123,7 @@ int initBoxMatrix (struct box *box, struct sudoku *sud, int boxPosVertical, int 
 int initBoxList(int dataDimension, struct sudoku *sud)
 {  
     int boxWidth = floor(sqrt((double) dataDimension));
+    sud->boxWidth = boxWidth;
     if ((boxWidth * boxWidth) != dataDimension)
     {
         printf("BoxWidth sqrt flooring failed to produce round number.");
@@ -119,30 +133,28 @@ int initBoxList(int dataDimension, struct sudoku *sud)
     //printf("\n dataDimension is %d, Boxwidth is:%d",dataDimension, boxWidth);
 
     // Init boxList
-    struct box**boxList = (struct box**)saferCalloc(boxWidth, sizeof(struct box*)); 
+    struct box**tempBoxList = (struct box**)saferCalloc(boxWidth, sizeof(struct box*)); 
     for (int i = 0; i < boxWidth; i++) // create boxList and assign pointer to a struct box
     {
-        boxList[i] = (struct box*)saferCalloc(boxWidth, sizeof(struct box));
+        tempBoxList[i] = (struct box*)saferCalloc(boxWidth, sizeof(struct box));
     }
     for (int row = 0; row < boxWidth; row++)
     {
         for (int col = 0; col < boxWidth; col++)
         {
-        struct box boxTemp;
-        boxTemp.unsolvedCount = 0;
-        initBoxMatrix(&boxTemp,sud, row,col, boxWidth);
-        boxList[row][col] = boxTemp;
+        struct box tempBox;
+        tempBox.unsolvedCount = MAXDIMENSION+1; // TODO: Check for errors in unsolvedCount logic by searching MAXDIMENSION+1
+        initBoxMatrix(&tempBox,sud, row,col, boxWidth);
+        tempBoxList[row][col] = tempBox;
         }
     }
-
-    // printf("boxList[0][1] = %i", *(boxList[2][2].pointerMatrix[1][1]));
-
-
+    sud->boxList = tempBoxList;
+    // printf("\nUnsolved count: %i", sud->boxList[0][2].unsolvedCount);
+    //printMatrix(*(sud->boxList[0][0].pointerMatrix), 3,3);
 
     return 0;
-
-
 }
+
 int initSudoku(int *size, int *dataDimension, int *sudokuArray,  struct sudoku *sud)
 {
     // assign one-dimensional attributes
@@ -160,12 +172,8 @@ int initSudoku(int *size, int *dataDimension, int *sudokuArray,  struct sudoku *
     printf("Sudoku Matrix: \n");
     sud->matrix = matrix;
     printMatrix(sud->matrix, sud->rowLength, sud->colLength);
-
     // Init boxStructure
     initBoxList(*dataDimension, sud);
-
-    // Calculate required length
-    
 
     return 0;
 }
@@ -209,21 +217,69 @@ int readFile(char *filename, int *dataCount, int *sudokuArray, int *matrixDimens
     return 0;
 }
 
-int solveSudoku()
+int checkRow()
+{
+    
+    return 0;
+}
+
+int checkCol()
+{
+
+    return 0;
+}
+
+int checkBox()
+{
+
+    return 0;
+}
+
+int solveSudoku(struct sudoku *sud)
 {
     // keep track of: steps taken, cycles taken
+    int totalUnsolved = 0;
+    for(int row = 0; row < sud->boxWidth; row++)
+    {
+        for (int col = 0; col < sud->boxWidth; col++)
+        {
+            totalUnsolved += sud->boxList[row][col].unsolvedCount;
+        }
+    
+    }
+    sud->totalUnsolved = totalUnsolved;
+    for (int number = 1; number <= sud->rowLength; number++)
+    {
+        checkRow();
+        checkCol();
+        checkBox(); // remember: Potential bug in pointer arithmetic
+    }
 
-    return 0;}
+    /*while (sud->totalUnsolved > 0){
 
-int outputSudoku()
+    }*/
+
+
+
+    return 0;
+}
+
+int outputSudoku(struct sudoku *sud)
 {
-    // print
+    if (sud->totalUnsolved == 0)
+    {
+    printf("\nSudoku Solved:\n");
+    printMatrix(sud->matrix,sud->rowLength,sud->colLength);
+    }
+    else
+    {
+        printf("Solution not found. Total unsolved: %i", sud->totalUnsolved);
+    }
 
     // save to disk
 
 
     return 0;}
-
 int main(void){
     //TODO: Enable queuing sudokus
     // Init vars
@@ -250,20 +306,9 @@ int main(void){
     // Convert one-dimensional temporary array to 2D matrix in sudoku struct
     struct sudoku *sud = (struct sudoku *) saferCalloc(1, sizeof(struct sudoku)); // initialize sud pointer to struct sudoku
     initSudoku(&dataCount,&matrixDimension, sudokuArray, sud) ;
-
-    // Free temporary variables; converted into struct sudoku
-
-    // solveSudoku
-    // printSudoku
-    // TODO: Free Malloc
-    solveSudoku();
-    outputSudoku();
+    solveSudoku(sud);
+    outputSudoku(sud);
     free(sud);
-    
-    // implement vertical line checker
-    // implement horizontal line checker
-    // implement box line checker
-
 
     return 0; // main finished succesfully 
 }
