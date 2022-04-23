0f84147007bcd51d4edbfd9be5e67caa4d5500e7
Kevin Focke <19843342+KevinFocke@users.noreply.github.com>
2022-04-23 07:48:37 +0000

    initial cTypes integration

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -1,6 +1,19 @@
-import SudokuSolverLib
+import ctypes
 
-SudokuSolverLib.startSudoku(algoChoice = 1, inputFilename = "Input_Cases/Individual/sudoku_input_difficult.txt", size = 0, dataDimension = 0)
+SudokuSolverLib = ctypes.CDLL('./SudokuSolverLib.so') 
+
+cAlgoChoiceValue = ctypes.c_int32(1) #Defaults to option 1, see algo overview in Readme
+
+# None, integers, bytes objects and (unicode) strings can be passed directly as parameters
+# However, the types are passed explicitly to avoid potential bugs.
+
+
+# Careful! The arguments should be provided positionally as in C!
+
+SudokuSolverLib.startSudoku(cAlgoChoiceValue)
+
+# Figure out how to pass array
+
 
 #inputFilename = "Input_Cases/Individual/sudoku_input_difficult.txt"
 #algoChoice = 1 ## The default algorithm is backtracking
